77d7b37ba73a9dc98ac14493d0f7c79336c3c136
dasmak-dev <franlopezescandell@gmail.com>
2021-03-17 19:05:22 +0100

    subir el proyecto 1ra ver.

---
+++ src/main/java/com/kardji/main/KardjiApplication.java
@@ -0,0 +1,13 @@
+package com.kardji.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KardjiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KardjiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kardji/main/KardjiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kardji.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KardjiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
